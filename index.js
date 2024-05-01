7e2c343ce18c527a94cfd0b45b2b268ccb5ac67d
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-05-01 16:22:29 -0500

    delete checked still doesnt work

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
+const chkdBtn = document.getElementById("checked-btn")
+
+
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -24,10 +27,13 @@ function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += 
-            `<li>
-                <a target='_blank' href='${leads[i]}'>
+            `<li class="list-item">
+                <input tabindex="-1" type="checkbox" class="delete=checkbox" name="list-item-checkbox">
+                <a target='_blank' href='${leads[i]}' class="a_tag" >
+                    
                 ${leads[i]}  
                 </a>
+        
             </li>`
     }
     ulEl.innerHTML = listItems
@@ -45,6 +51,48 @@ inputBtn.addEventListener("click", function() {
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
-
 })
 
+
+// if checked, remove item on button click
+
+document.getElementById("ul-el").addEventListener("click", function(e) {
+   
+    if (e.target && e.target.nodeName == "INPUT" ) {
+        console.log("list item ", e.target.id.replace("post-", ""), " was clicked!")
+        
+    }
+})
+
+
+chkdBtn.addEventListener("click", function(){
+    console.log("checked")
+
+    // var p = document.getElementById("ul-el")
+    var checkboxes = document.querySelectorAll("input[name=list-item-checkbox]:checked").forEach(e => e.remove());
+
+
+
+    console.log(checkboxes)
+
+    // for (var i = 0; i < checkboxes.length; i++) {
+
+        // if (checkboxes) {
+            // e.parentNode.parentNode.removeChild(e.parentNode);
+            
+
+        }
+        // if (checkboxes[i].querySelector("delete-checkbox").checked) {
+            // var p = document.getElementById("ul-el")
+            // var del = document.querySelector("li")
+            // p.removeChild(del)
+            // alert("removed")
+      
+        
+        
+   
+    
+    )
+
+
+    
